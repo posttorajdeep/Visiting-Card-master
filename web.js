33bc5ab23d4c0fdd1aa401bc5e910e42205355e7
Rajdeep Mishra <posttorajdeep@gmail.com>
2022-03-04 12:58:45 +0530

    First

---
+++ web.js
@@ -0,0 +1,16 @@
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  document.querySelector("#my").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.querySelector("#my").style.display = "block";
+
+}
+
+document.querySelector(".btn-primary").addEventListener("click",(e)=>{
+  document.querySelector("#list").classList.toggle("togl");
+})
+
